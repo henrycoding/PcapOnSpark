7a6e902d3a8f26614b45d01c4560039d9f8ce917
henrycoding <841636454@qq.com>
2019-01-22 18:21:20 +0800

    SYNC

--- src/Main.scala
+++ src/Main.scala
@@ -8,8 +8,14 @@ import org.apache.spark.input.PortableDataStream
 import org.apache.spark.{SparkConf, SparkContext}
 import io.pkts.filters.FilterException
 import io.pkts.framer.PcapFramer
-import io.pkts.packet.Packet
+import io.pkts.packet.{PacketFactory, Packet => PktsPacket}
+import io.pkts.packet.impl.PCapPacketImpl
 import io.pkts.protocol.Protocol
+import org.pcap4j.packet.{Packet, TcpPacket}
+import org.pcap4j.packet.factory.PacketFactories
+import org.pcap4j.packet.namednumber.DataLinkType
+
+case class PacketItem(payload: Array[Byte], pktLen: Long, arrTime: Long, dataLinkType: Int)
 
 import scala.collection.mutable.ArrayBuffer
 object Main {
@@ -19,22 +25,31 @@ object Main {
     val sc = SparkContext.getOrCreate(conf)
 
     val pcapFiles = sc.binaryFiles("hdfs://192.168.1.117:9000/zhangheng/testcap")
-    val t = pcapFiles.
+    pcapFiles.
       flatMap(t => {
-        val arr = new ArrayBuffer[Packet]()
+        val arr = new ArrayBuffer[PacketItem]()
         val cap = Pcap.openStream(t._2.open())
         cap.loop(new PacketHandler {
-          override def nextPacket(packet: Packet): Boolean = {
-            arr.append(packet)
+          override def nextPacket(packet: PktsPacket): Boolean = {
+            arr.append(PacketItem(packet.getPayload.getArray,
+              packet.asInstanceOf[PCapPacketImpl].getTotalLength,
+              packet.getArrivalTime,
+              ReflectUtils.getPrivateField[PcapGlobalHeader](cap, "header").getDataLinkType
+            ))
             true
           }
         })
         arr
       })
+      .repartition(1000)
       .map(t => {
-        HexDump.dumpHexString(t.getPayload.getArray)
+        PacketFactories.getFactory(classOf[Packet], classOf[DataLinkType]).newInstance(t.payload, 0, t.payload.length,
+          DataLinkType.getInstance(t.dataLinkType))
       })
-        .foreach(println)
+      .map(t => {
+
+      })
+      .foreach(println)
     sc.stop()
   }
 }
